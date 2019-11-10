13d6e1b00d2ee4136b6a40f20e4d513a42d8e152
JhonderStorm <jhonatan30pereira@gmail.com>
2019-11-10 03:19:32 -0300

    game over 0 vai pra menu, só isso memo

--- game_lucas/fases/fase_test.gd
+++ game_lucas/fases/fase_test.gd
@@ -1,5 +1,5 @@
 extends Node
-var enemie= preload("res://inimigos/inimigos_comun/fase_2/arqueiro/top_down/arqueiro(T_D).tscn")
+var enemie= preload("res://inimigos/inimigos_comun/fase_3/soldado_2_guerra/top_down/soldado_2_guerra_TD.tscn")
 # limite de inimigo vivo na cena
 var limit_enemie = atributos_fase_singleton.enemie_limit_in_game 
 var stop_create = atributos_fase_singleton.stop_spawn_enemie
--- game_lucas/scripts/script_game_over/game_over.gd
+++ game_lucas/scripts/script_game_over/game_over.gd
@@ -11,6 +11,8 @@ func _on_restart_game_pressed():
 	atributos_player_singleton.fire_stop = false
 	atributos_fase_singleton.anim_close_door_finalized = 0
 	
+	if (atributos_fase_singleton.fase_restart == 0):
+		get_tree().change_scene("res://cenas_globais/Menu_Inicial/menu_inicial.tscn")
 	if (atributos_fase_singleton.fase_restart == 1):
 		get_tree().change_scene("res://fases/fase_1/world_1_cena_1.tscn")
 	if (atributos_fase_singleton.fase_restart == 2):
